98689ec604443bb1d694d2e8802782a94414cb83
DimitriLibessart <dimitri.libessart@viacesi.fr>
2018-04-18 08:47:20 +0200

    notif v2

--- bde-app/public/js/notif.js
+++ bde-app/public/js/notif.js
@@ -1,20 +1,46 @@
-function getnotif(){
-    
-    //xml http request to /api/users
-    url = '';
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send(null);
-    
-    xhr.addEventListener('readystatechange', function() {
+var notif = [];
+var name;
+var desc;
 
-    //if request completed gets the JSON file
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      
-      
-      var response = JSON.parse(xhr.responseText);
-    
-    }
-});
+function getnotif() {
+
+    // function affichernotif() {
+
+    //     document.getElementById('nom').innerHTML = name;
+    //     document.getElementById('description').innerHTML = desc;
+
+    // }
+
+    var url = '/notification';
+
+    function executerequete() {
+
+        // if (notif.length === 0) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            
+
+            xhr.addEventListener('readystatechange', function () {
+
+                //if request completed gets the JSON file
+                if (xhr.readyState === 4 && xhr.status === 200) {
+
+                    var response = JSON.parse(xhr.responseText);
+
+                    name = response[0].name;
+                    desc = response[0].description;
+                    console.log(response);
+
+                    // callback();
+
+                    
+                }
+
+            });
+            xhr.send(null);
+        }
+    // }
+    executerequete();
 }
+
+getnotif();
