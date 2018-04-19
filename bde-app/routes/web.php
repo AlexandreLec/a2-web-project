649517609ea6fe43416eddeaab879cf2376340bd
Martinhubz <martinhub92@gmail.com>
2018-04-19 19:10:04 +0200

    report route

--- bde-app/routes/web.php
+++ bde-app/routes/web.php
@@ -77,6 +77,10 @@ Route::get('/events/soon/{id}','EventController@detail');
 Route::get('/idea/poll/add/{id}', 'EventIdeaController@addPoll');
 Route::get('/idea/poll/{id}', 'EventIdeaController@getPoll');
 
+//Report content
+Route::get('/report', function(){return view('report');});
+        
+
 //Shop routes
 Route::get('/shop', 'ShopController@index');
 
